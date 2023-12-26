a63b5fba520ded5d0a133abc40e3681981bacac2
o3-noone <jaloliddintuitdif315@gmail.com>
2023-12-26 22:42:35 +0500

    ok

--- js/main.js
+++ js/main.js
@@ -1,67 +1,36 @@
-const url = `https://fakestoreapi.com/products`;
-fetch(url)
-      .then(response => response.json())
-      .then(data => console.log(data))
-window.addEventListener("DOMContentLoaded", function () {
-  let 
-})
-const tabsParent = document.querySelector(".sec1__List");
-const tabs = document.querySelectorAll(".hero-item");
-const tabsContent = document.querySelectorAll(".tabcontent");
-let itemHeroText = document.querySelector(".item-nameChange");
-let textNameSlide = document.querySelector(".textName-Slide");
+const tabs = document.querySelectorAll(".sec4__Name-item");
+const tabsParent = document.querySelector(".sec4__Name-List");
+const tabsContent = document.querySelectorAll(".sec4__Cards");
 function hideTabContent() {
-  // tabsContent.forEach((item) => {
-  //   item.classList.add("DeActiveHeroItem");
-  //   item.classList.remove("ActiveHeroItem");
-  // });
+  tabsContent.forEach((item) => {
+    item.classList.remove("DeActiveContent");
+    item.classList.add("ActiveContent");
+  });
 
-  tabs.forEach((item) => {
-    item.classList.remove("ActiveHeroItem");
+  tabs.forEach((item, i) => {
+    item.classList.add("DeActive-Text");
+    item.classList.remove("Active-Text");
   });
 }
 
 function showTabContent(i = 0) {
-  // tabsContent[i].classList.add("ActiveHeroItem");
-  // tabsContent[i].classList.remove("DeActiveHeroItem");
-  tabs[i].classList.add("ActiveHeroItem");
-  tabs[i].classList.remove("DeActiveHeroItem");
+  tabsContent.forEach((item) => {
+    item.classList.remove("ActiveContent");
+    item.classList.add("DeActiveContent");
+  });
+
+  tabs[i].classList.add("Active-Text");
+  tabs[i].classList.remove("DeActive-Text");
+  tabsContent[i].classList.add("ActiveContent");
+  tabsContent[i].classList.remove("DeActiveContent");
 }
 
 hideTabContent();
 showTabContent();
 
-tabsParent.addEventListener("click", (event) => {
-  const target = event.target;
-  if (target && target.classList.contains("hero-item")) {
-    tabs.forEach((item, idx) => {
-      if (target === item) {
-        hideTabContent();
-        showTabContent(idx);
-
-      }
-    });
-  }
-});
-const sec1RightItem3 = document.querySelector(".sec1Right__item3");
-const tabsSec1Right = document.querySelector(".Right__items");
-// function showTabItem(i=0){
-// tabsSec1Right[i]
-// }
-// showTabItem()
-// sec1RightItem3.addEventListener("click", (event) => {
-//   const target = event.target;
-//   console.log(target);
-//   if (target && target.classList.contains("Right__items")) {
-//     tabsSec1Right.forEach((item, idx) => {
-//       if (target === item) {
-//        showTabItem(idx)
-//       }
-//     });
-//   }
-// });
-
-sec1RightItem3.addEventListener("click", (event) => {
-
-
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    hideTabContent();
+    showTabContent(index);
+  });
 });
